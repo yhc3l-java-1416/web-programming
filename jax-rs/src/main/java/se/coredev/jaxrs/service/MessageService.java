166ecb5b444a9c0b612a92a6e8d2aedea61711a9
Anders Carlsson <anders.carlsson@coredev.se>
2015-02-26 11:06:09 +0100

    new demo for JAX-RS

--- jax-rs/src/main/java/se/coredev/jaxrs/service/MessageService.java
+++ jax-rs/src/main/java/se/coredev/jaxrs/service/MessageService.java
@@ -13,7 +13,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import com.sun.corba.se.spi.activation.Repository;
+
 
 // http://127.0.0.1:8080/jax-rs/message
 @Path("message")
